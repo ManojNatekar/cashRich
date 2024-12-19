a0d0ebdfed86661da56374d31e02e8944b546d3e
Manoj Natekar <manojnatekar@Manojs-MacBook-Air.local>
2024-12-20 01:31:22 +0530

    adding ModelEntity class

---
+++ DemoApplication 2/src/main/java/com/example/demo/Entity/ModelEntity.java
@@ -0,0 +1,11 @@
+package com.example.demo.Entity;
+
+public class ModelEntity {
+    //this is just sample file added for testing purpose.
+
+    public String name;
+    public String address;
+    public int rollNumber;
+
+
+}
